71ad2cebec067af5afe4e7b13fae1fdb59f7d66e
Pavel Gorbokon aka pahanix <pavel.gorbokon@railsware.com>
2009-03-08 02:53:36 +0200

    Initial commit, 2 basic examples of human validation

---
+++ spec/base_spec.rb
@@ -0,0 +1,36 @@
+class Address < ActiveRecord::Base
+  
+  please_choose :country, :as => "your country"
+  and_fill :city, :name, :zip, [:adress, "Home Adress"]
+  and_make_sure :zip, :as => "Zip code", :is => [:uniq, :numeric, {:lenght => 5}, {:format => /^0\d{4}$/}]
+  and_also_make_sure :phone, :is => :numeric
+
+  otherwise "we can't bill you properly. Thank you!"
+end
+
+
+describe Address, "validation" do
+  it "should provide 1 error message" do
+    @address = Address.new
+    @address.should_not be_valid
+    @address.should have(1).errors_on(:human_error)    
+    @article.errors.full_messages.should == ["Please choose your country and fill City, Name, Zip code and Home Adress fields also please make sure that Zip code is uniq, numeric, more 5 characters long and Phone also is a number. Otherwise we can't bill you properly. Thank you!"]
+  end
+end
+
+class Article < ActiveRecord::Base
+  please_fill :title, :permalink
+end
+
+describe Article, "validation" do
+  before do
+    @article = Article.new
+  end
+
+  it "should provide error message 'Please fill Title and Permalink fields'" do
+    @article.should_not be_valid
+    @article.should have(1).errors_on(:human_error)
+    @article.errors.full_messages.should == ["Please fill Title and Permalink fields"]
+  end
+end
+
